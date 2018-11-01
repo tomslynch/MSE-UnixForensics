b489b85325c6f036416bc546cee7cff2152db5d5
Thomas Lynch <thomaslynch01@gmail.com>
2018-11-01 00:42:56 -0500

    added basic fork test

---
+++ tests/BasicForkTest.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Thomas Lynch on 11/1/18.
+//
+
+#include <iostream>
+#include <zconf.h>
+using namespace std;
+
+int main(){
+    pid_t processID = ::getpid();
+    printf("pid = %jd\n", (intmax_t) processID);
+    cout << "Press Enter to continue...";
+    cin.ignore();
+
+    fork();
+}
