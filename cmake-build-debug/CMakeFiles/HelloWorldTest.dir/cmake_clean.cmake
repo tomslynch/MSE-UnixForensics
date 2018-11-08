3aafd3ebfd041cc4b0d8a8dc7db03462c5d9e708
Thomas Lynch <thomaslynch01@gmail.com>
2018-11-08 11:27:04 -0600

    CMakeFiles directory dump

---
+++ cmake-build-debug/CMakeFiles/HelloWorldTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorldTest.dir/tests/HelloWorldTest.cpp.o"
+  "HelloWorldTest.pdb"
+  "HelloWorldTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloWorldTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ReadFIleTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadFileTest.dir/tests/ReadFileTest.cpp.o"
+  "ReadFileTest.pdb"
+  "ReadFileTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReadFileTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
